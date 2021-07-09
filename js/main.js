6323365f8838fe64ea0154f1be3afad79c42f19c
Thomas <thomas.new.it@gmail.com>
2021-07-09 20:48:36 +0500

    привязка переменных  js к html готова

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let numberOne = document.querySelector('.number-one');
+let numberTwo = document.querySelector('.number-two');
+
+let plusButton = document.querySelector('.plus');
+let minusButton = document.querySelector('.minus');
+let divideButton = document.querySelector('.divide');
+let multiplyButton = document.querySelector('.multiply');
+
+let clearButton = document.querySelector('.clear');
+
+let resultText = document.querySelector('.resut');
+
