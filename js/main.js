5741bc1b5a527c3075ee4b60012f45a96bedb397
Thomas <thomas.new.it@gmail.com>
2021-07-10 22:39:52 +0500

    add comment

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 let numberOne = document.querySelector('.number-one');
 let numberTwo = document.querySelector('.number-two');
 
+// присвоение переменных для кнопок операторов и кнопки очистки
 let plusButton = document.querySelector('.plus');
 let minusButton = document.querySelector('.minus');
 let divideButton = document.querySelector('.divide');
 let multiplyButton = document.querySelector('.multiply');
-
 let clearButton = document.querySelector('.clear');
 
+// присвоение переменной для строки с результатом 
 let resultText = document.querySelector('.result');
 
 // функция очистки
